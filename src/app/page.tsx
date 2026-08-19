af7c5e69cceed81071e76ef7c6029470eb5796d7
simply-kabir <k.kabir022007@gmail.com>
2026-08-19 23:13:06 +0530

    ios checkpoint 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 import Hero from "@/components/sections/hero/hero";
-import Projects from "@/components/sections/projects";
-import Skills from "@/components/sections/skills";
-import FoundationSection from "@/components/sections/foundation/FoundationSection";
-import ConnectSection from "@/components/sections/connect/ConnectSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Projects />
-      <Skills />
-      <FoundationSection />
-      <ConnectSection />
     </main>
   );
 }
